63b7486f7735fa56fbdf3ddd78c7ab546cfe2777
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-12-20 12:23:17 +0200

    make observables after variables not befor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataViewer1Component } from './data-viewer1/data-viewer1.component';
+import { DataViewer2Component } from './data-viewer2/data-viewer2.component';
+import { DataChangerComponent } from './data-changer/data-changer.component';
+import { CustomPaginatorComponent } from './custom-paginator/custom-paginator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'custom-paginator', component: CustomPaginatorComponent },
+  { path: 'data-viewer1', component: DataViewer1Component },
+  { path: 'data-viewer2', component: DataViewer2Component },
+  { path: 'data-changer', component: DataChangerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
